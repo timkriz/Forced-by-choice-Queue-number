cec99f22b0b8fc473f012343cbad338cc6491c00
timkriz <tim.kriznik@gmail.com>
2020-09-24 19:05:33 +0200

    ccc2

--- utils/users.js
+++ utils/users.js
@@ -1,12 +1,9 @@
 
-var clients = [{cookie:'5wwd1', username:'61', chatPeers: [3434], groupNumber:'002'}];
-var groups = [{id:'541', clients: [0,1,2,3,4,5,6,7,8,9,10, 11, 12, 13,14,15,16,17,18,19,20]}, {id:'356', clients: [
-  21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]}, 
-              {id:'987', clients: [
-                41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]}, {id:'002', clients: [
-                  61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79]}];
+var clients = [];
+var groups = [{id:'541', clients: [0,1,2,3,4,5,6,7,8,9,10, 11, 12, 13,14,15]}, {id:'356', clients: []}, 
+              {id:'987', clients: []}, {id:'002', clients: []}];
 
-var clientIDincrement = 80;
+var clientIDincrement = 16;
 var groupsSpot = 0;
 
 class client {
